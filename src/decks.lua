28eb9dcfd27a5815655bc54ac2e57f0a0072a86e
NeoKat <neokat75@gmail.com>
2026-02-01 00:00:51 +0300

    Reordered decks

--- src/decks.lua
+++ src/decks.lua
@@ -64,26 +64,39 @@ SMODS.Back {
     end
 }
 
--- Crumpled Deck
+-- Hoarder Deck
 SMODS.Back {
-    key = "crumpled",
+    key = "hoarder",
     atlas = "nancy_decks",
-    pos = { x = 3, y = 0 },
+    pos = { x = 4, y = 0 },
     discovered = true,
-    config = { joker_slot = -4, jokers = {'j_nancy_initiation'}, extra = { hsize = -1 } },
     loc_vars = function(self, info_queue, back)
-        return { vars = { self.config.extra.hsize, localize{type = 'name_text', key = self.config.jokers[1], set = 'Joker'} } }
+        return { vars = { colours = { G.C.RENTAL } } }
     end,
     calculate = function(self, back, context)
-        if context.card_added and context.card.ability.set == "Joker" then
-            G.jokers:change_size(1)
-            G.hand:change_size(self.config.extra.hsize)
+        if context.end_of_round and context.game_over == false and context.main_eval and context.beat_boss then
+            local editionless_jokers = SMODS.Edition:get_edition_cards(G.jokers, true)
+            if next(editionless_jokers) then
+                local eligible_card = pseudorandom_element(editionless_jokers, 'nancy_hoarderdeck')
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        eligible_card:set_edition('e_negative', true)
+                        return true
+                    end
+                }))
+                delay(1)
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        play_sound('gold_seal', 1.2, 0.4)
+                        eligible_card:juice_up()
+                        eligible_card:set_rental(true)
+                        return true
+                    end
+                }))
+            else
+                return { message = localize('nancy_notarget') }
+            end
         end
-        if (context.joker_type_destroyed or context.selling_card) and context.card.ability.set == "Joker" then
-            G.jokers:change_size(-1)
-            G.hand:change_size(-self.config.extra.hsize)
-        end
-        G.GAME.nancy_crumpledslots = G.GAME.starting_params.hand_size + G.hand.config.card_limits.mod
     end
 }
 
@@ -146,38 +159,25 @@ SMODS.Back {
     end
 }
 
--- Hoarder Deck
+-- Crumpled Deck
 SMODS.Back {
-    key = "hoarder",
+    key = "crumpled",
     atlas = "nancy_decks",
-    pos = { x = 4, y = 0 },
+    pos = { x = 3, y = 0 },
     discovered = true,
+    config = { joker_slot = -4, jokers = {'j_nancy_initiation'}, extra = { hsize = -1 } },
     loc_vars = function(self, info_queue, back)
-        return { vars = { colours = { G.C.RENTAL } } }
+        return { vars = { self.config.extra.hsize, localize{type = 'name_text', key = self.config.jokers[1], set = 'Joker'} } }
     end,
     calculate = function(self, back, context)
-        if context.end_of_round and context.game_over == false and context.main_eval and context.beat_boss then
-            local editionless_jokers = SMODS.Edition:get_edition_cards(G.jokers, true)
-            if next(editionless_jokers) then
-                local eligible_card = pseudorandom_element(editionless_jokers, 'nancy_hoarderdeck')
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        eligible_card:set_edition('e_negative', true)
-                        return true
-                    end
-                }))
-                delay(1)
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        play_sound('gold_seal', 1.2, 0.4)
-                        eligible_card:juice_up()
-                        eligible_card:set_rental(true)
-                        return true
-                    end
-                }))
-            else
-                return { message = localize('nancy_notarget') }
-            end
+        if context.card_added and context.card.ability.set == "Joker" then
+            G.jokers:change_size(1)
+            G.hand:change_size(self.config.extra.hsize)
         end
+        if (context.joker_type_destroyed or context.selling_card) and context.card.ability.set == "Joker" then
+            G.jokers:change_size(-1)
+            G.hand:change_size(-self.config.extra.hsize)
+        end
+        G.GAME.nancy_crumpledslots = G.GAME.starting_params.hand_size + G.hand.config.card_limits.mod
     end
 }
