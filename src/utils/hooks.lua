4d66c13f739e5a6f302016f08b6ad1647b2280cf
NeoKat <neokat75@gmail.com>
2026-02-04 03:29:44 +0300

    The Purse: Chiclets Gaming

--- src/utils/hooks.lua
+++ src/utils/hooks.lua
@@ -26,7 +26,7 @@ function CardArea:shuffle(_seed)
     -- execute and save the orig function's return
     local ret = shuffle(self, _seed)
     -- after the orig function
-    if G.GAME.blind and G.GAME.blind.config.blind.key == 'bl_nancy_purse' and not G.GAME.blind.disabled then
+    if G.GAME.blind and G.GAME.blind.config.blind.key == 'bl_nancy_purse' and not next(SMODS.find_card('j_chicot')) then
         -- I took this from All in Jest's Headstone.
         -- I don't understand why my code was crashing and failing to index 'card',
         -- but this doesn't crash when it's basically the exact same thing...
