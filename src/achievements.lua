760023dc428dcbe95d491a6118452d29e44e8258
NeoKat <neokat75@gmail.com>
2026-02-15 22:04:24 +0300

    Achievements done (with a patch :3)

--- src/achievements.lua
+++ src/achievements.lua
@@ -117,21 +117,21 @@ SMODS.Achievement {
 }
 
 SMODS.Achievement {
-    key = 'scoredeck',
-    bypass_all_unlocked = true,
-    -- reset_on_startup = true,
-    unlock_condition = function(self, args)
-        if args.type == "nancy_scoredeck" then return true end
-        return false
-    end
-}
-
-SMODS.Achievement {
     key = 'lowerreq',
     bypass_all_unlocked = true,
     -- reset_on_startup = true,
     unlock_condition = function(self, args)
         if args.type == "nancy_lowerreq" then return true end
+        return false
+    end
+}
+
+SMODS.Achievement {
+    key = 'scoredeck',
+    bypass_all_unlocked = true,
+    -- reset_on_startup = true,
+    unlock_condition = function(self, args)
+        if args.type == "nancy_scoredeck" then return true end
         return false
     end
 }
