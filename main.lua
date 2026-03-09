aa68ca9b29d6198e3d68f19e3ca199b41f404d6c
NeoKat <neokat75@gmail.com>
2026-03-09 22:37:43 +0300

    Mod icon

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
 NegaNancy = SMODS.current_mod
 
+-- Mod icon atlas
+SMODS.Atlas {
+    key = "modicon",
+    path = "icon.png",
+    px = 32,
+    py = 32
+}
+
 function NegaNancy.menu_cards()
     return {
 		{ key = 'j_nancy_negativenancy' },
